8d58b199ffedabb39edef975db384e61100a0980
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2024-03-04 13:21:34 +0530

    Add register function with required parameters

--- src/action/auth.ts
+++ src/action/auth.ts
@@ -35,7 +35,42 @@ export async function loginWithCredentials(email: string, password: string) {
   };
 }
 
-export async function register() {}
+export async function register({
+  name,
+  email,
+  password,
+  dob,
+  phone_no,
+  profile_img,
+  gender,
+  highest_edu,
+}: {
+  name: string;
+  email: string;
+  password: string;
+  dob: string;
+  phone_no: string;
+  profile_img: string;
+  gender: string;
+  highest_edu: string;
+}) {
+  const res = await fetch(' /User/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name,
+      email,
+      password,
+      dob,
+      phone_no,
+      profile_img,
+      gender,
+      highest_edu,
+    }),
+  });
+}
 
 export async function setEntrepreneurData() {}
 
