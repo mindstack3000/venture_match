103bb2c1c73d60338b99b1f1fdc5efb3102b0e79
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2024-03-05 03:41:49 +0530

    commit

--- src/components/Common/InvestmentsCard.tsx
+++ src/components/Common/InvestmentsCard.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
-import Reliance from "../../assets/Reliance.png"
+import Reliance from '../../assets/Reliance.png';
 interface InvestmentCardProps {
   equity: string;
 }
 
 const InvestmentsCard: React.FC<InvestmentCardProps> = ({ equity }) => {
   return (
-    <div className=" bg-slate-200 p-2 flex space-x-4  rounded-3xl max-w-md mx-auto relative  items-center">
-      <Image src={Reliance} alt="" className="flex w-[60px] h-[60px] border-gray-400"/>
-      <div className=" space-x-4">
-        <div>
-          <h3 className="text-lg font-medium flex-shrink mx-4 ">Reliance Industries</h3>
-          <p className="bg-yellow-300 rounded-3xl w-fit px-2 text-base mt-1">{equity}   Equity</p>
+    <div className=" bg-slate-200 p-5 flex space-x-4 w-1/2 rounded-3xl mx-auto relative justify-between  items-center">
+      <div className="flex items-center justify-center gap-5">
+        <Image
+          src={Reliance}
+          alt=""
+          className="flex w-[60px] h-[60px] border-gray-400"
+        />
+        <div className=" space-x-4">
+          <div>
+            <h3 className="text-lg font-medium flex-shrink mx-4 ">
+              Reliance Industries
+            </h3>
+            <p className="bg-yellow-300 rounded-3xl w-fit px-2 text-base mt-1">
+              {equity} Equity
+            </p>
+          </div>
         </div>
       </div>
-      <div>
-        $1234
-      </div>
+      <div>$1234</div>
     </div>
   );
 };
--- src/pages/InvestorDashboard.tsx
+++ src/pages/InvestorDashboard.tsx
@@ -1,8 +1,8 @@
-import InvestmentsCard from "../components/Common/InvestmentsCard";
-import { Command, Settings, Filter } from "lucide-react";
-import Data from "@/app/marketplace/data";
-import Image from "next/image";
-import PieChart from "../assets/PieChart.png";
+import InvestmentsCard from '../components/Common/InvestmentsCard';
+import { Command, Settings, Filter } from 'lucide-react';
+import Data from '@/app/marketplace/data';
+import Image from 'next/image';
+import PieChart from '../assets/PieChart.png';
 
 function InvestorDashBoard() {
   return (
@@ -11,8 +11,8 @@ function InvestorDashBoard() {
         <div className=" flex items-center p-4 justify-between text-white text-xl w-full bg-primary">
           <span className="flex flex-col p-2">
             <p className="font-semibold capitalize">Welcome back,</p>
-            <p className="font-bold text-4xl capitalize">{`${"Elon Musk"}`}</p>
-            <p className="font-light text-xl">{`${"elommusk@mail.com"}`}</p>
+            <p className="font-bold text-4xl capitalize">{`${'Elon Musk'}`}</p>
+            <p className="font-light text-xl">{`${'elommusk@mail.com'}`}</p>
           </span>
 
           <p className="uppercase text-nowrap p-2">
@@ -20,10 +20,11 @@ function InvestorDashBoard() {
             <span className="font-bold text-3xl">{`${Data.length}`}</span>
           </p>
         </div>
+
         <div className="flex justify-center items-center w-full h-full">
-          <div className="border-2 flex md:flex-row flex-col border-black w-[95%] py-2 h-full">
+          <div className="border-2 flex md:flex-row flex-col border-black w-[20%] py-2 h-full">
             {/* SideNav */}
-            <div className="md:w-[20%] w-full h-fit md:rounded-md md:h-full flex md:p-0 p-3 justify-between md:justify-evenly md:flex-col md:ml-2 bg-primary-100 ">
+            <div className="md:w-full w-full h-fit md:rounded-md md:h-full flex md:p-0 p-3 justify-between md:justify-evenly md:flex-col md:ml-2 bg-primary-100 ">
               <div className="flex flex-col gap-4">
                 <p className="flex w-32 ml-auto mr-auto">
                   <Command />
@@ -43,15 +44,27 @@ function InvestorDashBoard() {
               </div>
             </div>
           </div>
-          <h1 className="uppercase text-3xl font-semibold">companies</h1>
-          <div className="w-full gap-4 md:w-[90%] flex items-center flex-col">
-            <InvestmentsCard equity="30" />
-            <InvestmentsCard equity="30" />
-            <InvestmentsCard equity="30" />
-            <InvestmentsCard equity="30" />
-            <InvestmentsCard equity="30" />
-            <InvestmentsCard equity="30" />
-            <InvestmentsCard equity="30" />
+
+          <div className=" w-full flex flex-col items-center justify-center gap-10 overflow-scroll">
+            <h1 className="uppercase text-3xl font-semibold mt-20">
+              companies
+            </h1>
+            <div className="w-full gap-4 md:w-[90%] flex items-center flex-col ">
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              <InvestmentsCard equity="30" />
+              dfh
+            </div>
           </div>
         </div>
       </div>
