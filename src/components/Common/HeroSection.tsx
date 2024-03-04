b2715546fe4a2326b7aebedafc4b340580111c40
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2024-03-04 16:29:05 +0530

    Refactor HeroSection component to use next/image for image rendering

--- src/components/Common/HeroSection.tsx
+++ src/components/Common/HeroSection.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
 import { Button } from '../ui/button';
-import heroImg from '/images/heroImg.svg';
 
 const HeroSection = () => {
   return (
-    <div className="relative w-full my-3 bg-slate-200">
-      <div
-        className="absolute inset-0 bg-gradient-to-r from-primary-100 to-primary-200"
-        style={{
-          backgroundImage: `url(${heroImg})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      />
-      <div className="relative z-10 text-white text-center">
-        <h1 className="text-4xl">Fuel the change</h1>
-        <h1 className="text-4xl">you want to see</h1>
-        <Button className="bg-primary-100 mt-4">Get Started</Button>
+    <div className="w-full my-3 bg-slate-200 ">
+      <div className="w-full h-96 bg-gradient-to-r from-primary-100 to-primary-200">
+        <Image
+          alt="hero"
+          src={'/images/heroImg.svg'}
+          width={100}
+          height={100}
+          className=" object-cover w-full h-96"
+        />
       </div>
+      <h1 className="text-4xl text-white">Fuel the change </h1>
+      <h1 className="text-4xl text-white">you want to see</h1>
+      <button className="bg-primary-100 text-white font-bold px-4 py-2 my-4">
+        Get Started
+      </button>
     </div>
   );
 };
