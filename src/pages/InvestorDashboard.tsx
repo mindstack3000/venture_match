96d653d08ffef37f05ea2016e1af8fbc2256fa64
Freakyab <aryanbhisikar1@gmail.com>
2024-03-04 15:22:55 +0530

    Add InvestorDashboard and MarketPage components

---
+++ src/pages/InvestorDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InvestorDashboard() {
+  return (
+    <div>InvestorDashboard</div>
+  )
+}
+
+export default InvestorDashboard
---
+++ src/pages/MarketPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MarketPage() {
+  return (
+    <div>MarketPage</div>
+  )
+}
+
+export default MarketPage;
