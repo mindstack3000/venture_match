bede6c3c2ab64bbecccac2d627cdc1aff15e47b5
sauravbhukte2308@gmail.com <sauravbhukte2308@gmail.com>
2024-03-05 01:30:37 +0530

    investor dashboard

--- src/pages/InvestorDashboard.tsx
+++ src/pages/InvestorDashboard.tsx
@@ -1,46 +1,86 @@
 import InvestmentsCard from "../components/Common/InvestmentsCard";
-import { FaVectorSquare } from "react-icons/fa";
-import { IoSettingsOutline } from "react-icons/io5";
-import { CiFilter } from "react-icons/ci";
+import { Command, Settings, Filter } from "lucide-react";
+import Data from "@/app/marketplace/data";
+import Image from "next/image";
+import PieChart from "../assets/PieChart.png";
 
 function InvestorDashBoard() {
   return (
     <>
-      <div className="bg-green-950 space-x-4">
-        <div className="left text-white">
-          <div className="text-xl ">Welcome Back</div>
-          <div className="text-3xl">Elon Musk</div>
-          <div>elonmmusk@gmail.com</div>
+      <div className="h-screen">
+        <div className=" flex items-center p-4 justify-between text-white text-xl w-full bg-primary">
+          <span className="flex flex-col p-2">
+            <p className="font-semibold capitalize">Welcome back,</p>
+            <p className="font-bold text-4xl capitalize">{`${"Elon Musk"}`}</p>
+            <p className="font-light text-xl">{`${"elommusk@mail.com"}`}</p>
+          </span>
+
+          <p className="uppercase text-nowrap p-2">
+            <span>total companies : </span>
+            <span className="font-bold text-3xl">{`${Data.length}`}</span>
+          </p>
         </div>
-        <div className="right-0 text-white">Total Companies:2</div>
-      </div>
-      <div className="bg-primary-200 h-[800px] w-[300px] ">
-        <div>
-          <FaVectorSquare />
-          Companies
-        </div>
-        <div>
-          <IoSettingsOutline />
-          Settings
-        </div>
-              <p className="h-px my-8 bg-black border-0 dark:bg-gray-700"/>
-              <div>
-              <CiFilter />
-                Filter
+        <div className="flex justify-center items-center w-full h-full">
+          
+          <div className="border-2 flex md:flex-row flex-col border-black w-[95%] py-2 h-full">
+            {/* SideNav */}
+            <div className="sideNav w-auto">
+            <div className="md:w-[20%] w-full h-fit md:rounded-md md:h-full flex md:p-0 p-3 justify-between md:justify-evenly md:flex-col md:ml-2 bg-primary-100 ">
+              <div className="flex flex-col gap-4">
+                <p className="flex w-32 ml-auto mr-auto">
+                  <Command />
+                  <span className="pl-2 font-bold">Companies</span>
+                </p>
+                <p className="flex w-32 ml-auto mr-auto">
+                  <Settings />
+                  <span className="pl-2 font-bold">Settings</span>
+                </p>
               </div>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <InvestmentsCard equity="30" />
-      </div>
-      <div className="bg-primary-200 h-[800px] w-[300px] p-3">
-        <span className=" flex bg-black text-white p-3 rounded-lg w-[200px] h-[100px]">
-          <div> $124533 </div>
-          <div> Investment</div>
-        </span>
-        <span className=" flex bg-black text-white p-3 rounded-lg w-[200px] h-[100px]">
-          <div> 45 </div>
-          <div className="flex right-0 bottom-0"> Companies</div>
-        </span>
+              <div className="w-full transform -translate-x-2 bg-black border-2 h-[1px] border-black hidden md:flex"></div>
+              <div>
+                <p className="flex w-32 ml-auto mr-auto">
+                  <Filter />
+                  <span className="pl-2 font-bold">Filter</span>
+                </p>
+              </div>
+            </div>
+            </div>
+            <div className="cards">
+            <div className="overflow-auto m-4">
+              <h1 className="uppercase text-3xl font-semibold">companies</h1>
+              <div className="w-full gap-4 md:w-[90%] flex items-center flex-col">
+                <InvestmentsCard equity="30" />
+                <InvestmentsCard equity="30" />
+                <InvestmentsCard equity="30" />
+                <InvestmentsCard equity="30" />
+                <InvestmentsCard equity="30" />
+                <InvestmentsCard equity="30" />
+                <InvestmentsCard equity="30" />
+              </div>
+            </div>
+            </div>
+            <div className="right-0">
+            <div className="md:w-[20%] w-full h-fit md:rounded-md md:h-full flex md:p-0 p-3 justify-between md:justify-evenly md:flex-col md:ml-2 bg-primary-100 ">
+              <div>
+                <p className="flex w-32 h-32 ml-auto mr-auto">
+                <Image src={PieChart} alt="" className=" w-[200px] h-[200px]" />
+                  <span className="pl-2 font-bold"></span>
+                </p>
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="flex bg-black text-2xl object-contain font-bold rounded-md text-white w-[200px] h-[100px] ml-auto mr-auto">
+                  12345$
+                  <span className="pl-2 font-bold">Investments</span>
+                </p>
+                <p className="flex  bg-black text-3xl font-bold rounded-md text-white w-[200px] h-[100px] ml-auto mr-auto">
+                  45
+                  <span className="pl-2 text-2xl">Companies</span>
+                </p>
+              </div>            
+            </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
