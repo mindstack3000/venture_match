6a657800d3fe234c07c4d0c447c1ab196381dd72
Freakyab <aryanbhisikar1@gmail.com>
2024-03-04 15:42:12 +0530

    Add vercel.json and fix register API URL

--- src/action/auth.ts
+++ src/action/auth.ts
@@ -56,7 +56,7 @@ export async function register({
 }) {
   console.log('registering');
   // console.log(name)
-  const res = await fetch(' https://venture-match-backend.vercel.app/User/register', {
+  const res = await fetch('https://venture-match-backend.vercel.app/user/register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
