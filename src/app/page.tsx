7184ecad39ef7a1a2e4f84d3568aca7679492f66
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2024-03-04 15:30:24 +0530

    Add homepage and dashboard page, implement log out functionality, and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import LoginForm from '@/components/Auth/LoginForm';
 import Footer from '@/components/Common/Footer';
+import HomePage from '@/pages/HomePage';
 
 export default function Home() {
   return (
     <main>
-      <LoginForm />
-      <Footer />
+      <HomePage />
     </main>
   );
 }
