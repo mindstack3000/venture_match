8d5ed86567c43d40ef8fd21ae072c57834b32c19
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2024-03-04 18:02:50 +0530

    Refactor component imports and update file paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import EntrepreneurForm from "@/components/Auth/EntrepreneurForm";
-import LoginForm from "@/components/Auth/LoginForm";
-import Footer from "@/components/Common/Footer";
+import EntrepreneurForm from '@/components/Form/EntrepreneurForm';
+import LoginForm from '@/components/Auth/LoginForm';
+import Footer from '@/components/Common/Footer';
 
 export default function Home() {
   return (
